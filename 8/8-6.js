53cb8d513ba968ff45a36392e082398c98a042a1
krisstya2000 <65007958+krisstya2000@users.noreply.github.com>
2020-05-19 21:14:49 +0700

    Update 8-6.js

--- 8/8-6.js
+++ 8/8-6.js
@@ -1,17 +1,13 @@
 const readlineSync = require("readline-sync");
 
-let str = readlineSync.question("str?\n> ");
-let words = str.split(" ");
+let word = readlineSync.question("word?\n> ");
+let revWord = "";
 
-for (let word of words) {
-    let revWord = "";
-    for (let i = word.length - 1; i >= 0; --i) {
-      revWord += word[i];
-    }
-    if ( revWord == word ) {
-        console.log( true );
-    } else {
-        console.log(false);
-    }
+for (let i = word.length - 1; i >= 0; --i) {
+    revWord += word[i];
+  }
+if ( revWord == word ) {
+    console.log( true );
+} else {
+    console.log(false);
 }
-
